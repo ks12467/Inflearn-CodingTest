75379a928f5452f2e75a9f3b722dae35a2157e17
HyeonLo <ks12467@naver.com>
2024-09-18 09:33:02 +0900

    Beakjoon_11689 오일러 피 함수 구현하기

---
+++ src/hard/Beakjoon_11689.java
@@ -0,0 +1,27 @@
+package hard;
+
+//오일러 피 함수 구현하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon_11689 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long result = n;
+        for(long p = 2; p <= Math.sqrt(n); p++){
+            if(n % p == 0){
+                result = result - result / p;
+                while(n % p == 0){
+                    n /= p;
+                }
+            }
+        }
+        if(n > 1){
+            result = result - result / n;
+        }
+        System.out.println(result);
+    }
+}
