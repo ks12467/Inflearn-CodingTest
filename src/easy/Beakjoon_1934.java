05a2e09230f488e63cd8a89c74339140e9100ade
HyeonLo <ks12467@naver.com>
2024-09-21 09:14:32 +0900

    Beakjoon_1934 최소 공배수 구하기

---
+++ src/easy/Beakjoon_1934.java
@@ -0,0 +1,26 @@
+package easy;
+
+//최소 공배수 구하기
+
+import java.util.Scanner;
+
+public class Beakjoon_1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a * b / gcd(a, b);
+            System.out.println(result);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
