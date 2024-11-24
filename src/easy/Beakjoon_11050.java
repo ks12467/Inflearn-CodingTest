7df057bd9a874035c458e521fd3db359689fd022
HyeonLo <ks12467@naver.com>
2024-11-24 09:27:13 +0900

    Beakjoon_11050 이항 계수 구하기 1

---
+++ src/easy/Beakjoon_11050.java
@@ -0,0 +1,28 @@
+package easy;
+
+//이항 계수 구하기 1
+
+import java.util.Scanner;
+
+public class Beakjoon_11050 {
+    static int N, K;
+    static int[][] D;
+
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        D = new int[N + 1][N + 1];
+        for(int i = 0; i<= N; i++){
+            D[i][1] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
+        }
+        for(int i = 2; i<= N; i++){
+            for(int j = 1; j < i; j++){
+                D[i][j] = D[i -1][j] + D[i -1][j -1];
+            }
+        }
+        System.out.println(D[N][K]);
+    }
+}
